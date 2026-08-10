3a64e1c804dd375fbc0ed7cb474add98224b3744
Ashwin K <ashwinkrishnan48@gmail.com>
2026-08-10 11:20:54 +0530

    Use DATABASE_URL for postgres

--- backend/db.js
+++ backend/db.js
@@ -28,12 +28,16 @@
 
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 // const pool = new Pool({
